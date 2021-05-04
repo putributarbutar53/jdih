f3e2c1c616887bffcd5b753c73555987a1c3f91d
Johannes Christian Sitorus <jsitorus6@gmail.com>
2021-05-04 14:02:56 +0700

    main

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -22,8 +22,9 @@ return [
         ],
         'user' => [
             'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
+            'enableAutoLogin' => false,
             'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+            'authTimeout' => 1800,
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
